4b5c3c32d1c8247f4698f401656bbe5b980a7912
Rebecca Strauss <rstrauss127@gmail.com>
2019-05-14 18:49:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def cart 
+    return session[:cart]
+  end
+
   def add_to_cart
     #get item from the path POST /items/:id/add_to_cart
     @item = Item.find(params[:id])
