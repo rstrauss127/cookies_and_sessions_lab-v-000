ab5302c887397a87e39876fa5b3ea10a62efa460
Rebecca Strauss <rstrauss127@gmail.com>
2019-05-14 19:03:03 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,20 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def cart 
-    cart = session[:cart] || []
-    return cart
+  def cart
+    session[:cart] ||= []
   end
 
   def add_to_cart
     #get item from the path POST /items/:id/add_to_cart
     @item = Item.find(params[:id])
-    
+
     #Load the cart from the session, or creat a new empty one
     cart = session[:cart] || []
-    cart << @item.id 
+    cart << @item.id
 
-    #save the cart in the session 
+    #save the cart in the session
     session[:cart] = cart
   end
 end
