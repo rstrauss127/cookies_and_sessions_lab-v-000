b03fd34a4467b93a3ffd7f1e4693f30b447d5ba3
Rebecca Strauss <rstrauss127@gmail.com>
2019-05-14 19:31:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,15 +7,5 @@ class ApplicationController < ActionController::Base
     session[:cart] ||= []
   end
 
-  def add_to_cart
-    #get item from the path POST /items/:id/add_to_cart
-    @item = Item.find(params[:id])
-
-    #Load the cart from the session, or creat a new empty one
-    cart = session[:cart] || []
-    cart << @item.id
-
-    #save the cart in the session
-    session[:cart] = cart
-  end
+  
 end
